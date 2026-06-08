4ad6810bbf976de0effb3c861f7bbf6cde09bbde
Diego <analistasistemas@gmail.com>
2026-06-08 13:50:16 -0300

    feat(redesign): monta ThemeProvider + script anti-flash (dark padrao)

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import App from './App';
+import { ThemeProvider } from '@/components/theme-provider';
 import { useAuthStore } from '@/stores/auth-store';
 import './index.css';
 
@@ -14,7 +15,9 @@ useAuthStore.getState().hydrate();
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={qc}>
-      <App />
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
     </QueryClientProvider>
   </StrictMode>
 );
